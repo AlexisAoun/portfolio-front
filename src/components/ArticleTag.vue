d5f6062bed20ddd511c76a23f11205d6c80ab701
Alexis Aoun <alexis.aoun@hotmail.com>
2022-11-30 20:39:29 +0100

    feat: vue app base + tag component

---
+++ src/components/ArticleTag.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tag" :style="'background-color:' + color + ';'+'border-color:' + borderColor + ';'">{{ title }}</div>
+</template>
+<script type="module">
+export default {
+  name: 'articleTag',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: '#ff0000',
+    },
+    borderColor: {
+      type: String,
+      default: '#ff0000',
+    },
+  },
+}
+</script>
+<style>
+.tag {
+  display: inline-block;
+  border: solid 3px;
+  border-radius: 1rem;
+  padding: 0.3rem;
+}
+</style>
